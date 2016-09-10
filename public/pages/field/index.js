519529789f4169c3b9929943fffccbdf65446ee6
youyou <youyou@meili-lic.com>
2016-09-10 15:17:52 +0800

    添加表单组件

--- public/pages/field/index.js
+++ public/pages/field/index.js
@@ -1,16 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import FieldComponent from 'source_path/field/index';
+import FieldReadme from './README.md';
 
-export default class FieldView extends Component {
+//详细配置信息见test.js
+import testData from './test.js'
+import './index.less'
+
+export default class TipView extends Component {
 	constructor () {
 		super();
-		this.state = {};
 	}
-	componentDidMount () {
-		
+	
+	//获取表单数据
+	getData(){
+		var data = FieldComponent.getData("textForm");
+		console.info(data);
 	}
+
+	//初始化表单数据
+	resetData(){
+		FieldComponent.resetData("textForm");
+	}
+
+	//清除表单数据
+	clearData(){
+		FieldComponent.clearData("textForm");
+	}
+
+	//查询表单改变
+    onQueryChange(data){
+        console.info(data);
+    }
+
 	render () {
 		return (
-			<div>Field</div>
-		)
+			<div className="m-l m-r m-b-xxl">
+				<h1>
+					表单 - Field
+				</h1>
+				<h2>
+					1. 示例
+				</h2>
+				<div className="m-b m-t">
+					<button
+						className="btn btn-success-custom w-sm m-r" 
+						onClick={ (e) => { this.getData() }}>
+						获取数据
+					</button>
+					<button
+						className="btn btn-info-custom w-sm m-r" 
+						onClick={ (e) => { this.resetData() }}>
+						初始化数据
+					</button>
+					<button
+						className="btn btn-warning-custom w-sm m-r" 
+						onClick={ (e) => { this.clearData() }}>
+						清除数据
+					</button>
+					<FieldComponent 
+						data = {testData} 
+						onQuery={this.onQueryChange.bind(this)} 
+						prefixcls="field-test"
+						form='textForm'/>
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: FieldReadme }}>
+					
+				</div>
+			</div>
+	  	)
 	}
 }
