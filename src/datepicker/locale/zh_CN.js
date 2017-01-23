db97adf508f98e3e09786dd1b1225ef80a24a6c6
youyou <youyou@meili-lic.com>
2017-01-23 10:44:03 +0800

    修改时间组件

--- src/datepicker/locale/zh_CN.js
+++ src/datepicker/locale/zh_CN.js
@@ -5,11 +5,10 @@ import CalendarLocale from 'rc-calendar/lib/locale/zh_CN';
 // 统一合并为完整的 Locale
 const locale = objectAssign({}, GregorianCalendarLocale);
 locale.lang = objectAssign({
-	placeholder:'请选择日期',
-	timePlaceholder:'请选择时间',
+    placeholder: '请选择日期',
+    timePlaceholder: '请选择时间'
 }, CalendarLocale);
 
-// should add whitespace between char in Button
 locale.lang.ok = '确 定';
 
 export default locale;
