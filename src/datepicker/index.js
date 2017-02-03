75560ed54ba7941b10776b18265cea4c001367ff
youyou <youyou@meili-lic.com>
2017-02-03 15:26:48 +0800

    时间组件

--- src/datepicker/index.js
+++ src/datepicker/index.js
@@ -2,18 +2,42 @@ import React, { Component, PropTypes } from 'react';
 import RCCalendar from 'rc-calendar';
 import RCDatePicker from 'rc-calendar/lib/Picker';
 import GregorianCalendar from 'gregorian-calendar';
-import PickerMixin from './PickerMixin';
 import TimePicker from 'rc-time-picker';
 import classNames from 'classnames';
+import objectAssign from 'object-assign';
+import DateTimeFormat from 'gregorian-calendar-format';
+import defaultLocale from './locale/zh_CN';
 
 import './style/index.less';
 
 export default class DatePicker extends Component {
-    mixins: [ PickerMixin ]
+    static defaultProps = {
+        format: 'yyyy-MM-dd',
+        transitionName: 'slide-mc', //
+        popmcStyle: {}, //
+        align: { offset: [0, -9] }, //
+        style: {},
+        disabled: false,
+        timeConfig: {}, //
+        transitionName: 'slide-mc',
+        onChange() {},  // onChange 可用于 Validator
+        locale: {}
+    };
+
+    static propTypes = {
+        format : PropTypes.string, 
+        transitionName: PropTypes.string, //
+        popmcStyle: PropTypes.object, //
+        align: PropTypes.object, //
+        style: PropTypes.object, //
+        disabled: PropTypes.bool,
+        timeConfig: PropTypes.object, //
+        transitionName: PropTypes.string,
+        locale: PropTypes.object
+    };
 
     constructor (props) {
         super(props);
-
         this.state = {
             value: this.parseDateFromValue(props.value || props.defaultValue),
             open: false
@@ -21,39 +45,81 @@ export default class DatePicker extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-          if ('value' in nextProps) {
+        if ('value' in nextProps) {
             this.setState({
-                  value: this.parseDateFromValue(nextProps.value)
+                value: this.parseDateFromValue(nextProps.value)
             });
-          }
+        }
     }
 
     handleChange(value) {
-          if (!('value' in this.props)) {
+        if (!('value' in this.props)) {
             this.setState({ value });
-          }
-          const timeValue = value ? new Date(value.getTime()) : null;
-          this.props.onChange(timeValue);
+        }
+        const timeValue = value ? new Date(value.getTime()) : null;
+        this.props.onChange(timeValue);
     }
 
     getFormatTime( value ){
-          let defaultDate = new Date();
-          let timeReg = /^(\d{2}):(\d{2}):(\d{2})$/;
+        let defaultDate = new Date();
+        let timeReg = /^(\d{2}):(\d{2}):(\d{2})$/;
 
-          if( !value ){
+        if( !value ){
             return defaultDate.getTime();
-          }
+        }
 
-          if( !timeReg.test(value) ){
+        if( !timeReg.test(value) ){
             console.error('Time format must be "xx:xx:xx"!');
             return defaultDate.getTime();
-          }
+        }
 
         defaultDate.setHours( Number( RegExp.$1) );
         defaultDate.setMinutes( Number( RegExp.$2) );
-          defaultDate.setSeconds( Number( RegExp.$3) );
+        defaultDate.setSeconds( Number( RegExp.$3) );
 
-          return defaultDate.getTime();
+        return defaultDate.getTime();
+    }
+
+    getLocale () {
+        // 统一合并为完整的 Locale
+        const locale = objectAssign({}, defaultLocale, this.props.locale);
+        locale.lang = objectAssign({}, defaultLocale.lang, this.props.locale.lang);
+        return locale;
+    }
+
+    getFormatter () {
+        const formats = this.formats = this.formats || {};
+        const format = this.props.format;
+        if (formats[format]) {
+            return formats[format];
+        }
+        formats[format] = new DateTimeFormat(format, this.getLocale().lang.format);
+        console.info(format);
+        return formats[format];
+    }
+
+    parseDateFromValue (value) {
+        if (value) {
+            if (typeof value === 'string') {
+                return this.getFormatter().parse(value, { locale: this.getLocale() });
+            } else if (value instanceof Date) {
+                const date = new GregorianCalendar(this.getLocale());
+                date.setTime(+value);
+                return date;
+            }
+        } else if (value === null) {
+            return value;
+        }
+        return undefined;
+    }
+
+    // remove input readonly warning
+    handleInputChange () {}
+
+    toggleOpen (e) {
+        this.setState({
+            open: e.open
+        });
     }
 
     render () {
@@ -104,7 +170,6 @@ export default class DatePicker extends Component {
         if (this.state.open) {
             pickerClass += ' mc-calendar-picker-open';
         }
-
         return (
             <span className={pickerClass}>
                 <RCDatePicker
@@ -115,9 +180,9 @@ export default class DatePicker extends Component {
                     prefixCls='mc-calendar-picker-container'
                     style={this.props.popmcStyle}
                     align={this.props.align}
-                    onOpen={this.toggleOpen}
-                    onClose={this.toggleOpen}
-                    onChange={this.handleChange}>
+                    onOpen={(e) => this.toggleOpen(e)}
+                    onClose={(e) => this.toggleOpen(e)}
+                    onChange={(value) => this.handleChange(value)}>
 
                     {({value }) => {
                         return (
@@ -139,21 +204,3 @@ export default class DatePicker extends Component {
         );
     }
 }
-
-DatePicker.defaultProps = {
-    transitionName: 'slide-mc', //
-    popmcStyle: {}, //
-    align: { offset: [0, -9] }, //
-    style: {},
-    disabled: false,
-    timeConfig: {} //
-};
-
-DatePicker.propTypes = {
-    transitionName: PropTypes.string, //
-    popmcStyle: PropTypes.object, //
-    align: PropTypes.object, //
-    style: PropTypes.object, //
-    disabled: PropTypes.boolean,
-    timeConfig: PropTypes.object //
-};
