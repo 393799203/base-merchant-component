38d89556d0a78f67707d81edab22bf55e49dd9fb
chenxuan <chenxuan@mogujie.com>
2017-02-09 16:44:26 +0800

    es6

--- src/tag/index.js
+++ src/tag/index.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import './style/index.less';
 
-var Tag = React.createClass({
-    propTypes: {
-        color: React.PropTypes.string,
-        closeable: React.PropTypes.bool,
-        afterClose: React.PropTypes.func,
-        circle: React.PropTypes.bool
-    },
+export default class Tag extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {};
+    }
 
-    close(e){
-        e.target.parentNode.style.display = "none";
+    close (e) {
+        e.target.parentNode.style.display = 'none';
         this.props.afterClose ? this.props.afterClose() : null;
-    },
+    }
 
-    render(){
-        let {color,afterClose,circle,closeable} = this.props;
+    render () {
+        let { color, afterClose, circle, closeable } = this.props;
         let clsName = 'wrap ';
-        clsName += ['red','blue','green'].indexOf(color) == -1 ? 'default' : color;
+        clsName += ['red', 'blue', 'green'].indexOf(color) === -1 ? 'default' : color;
         clsName += circle ? ' circle' : '';
         return (<div className={clsName}>
-          {this.props.children}
+          { this.props.children }
           {
-            closeable ? (<span className='closeBtn' onClick={this.close}>x</span>) : null
+            closeable ? (<span className = 'closeBtn' onClick = { this.close.bind(this) }>x</span>) : null
           }
         </div>);
     }
-});
+}
 
-module.exports = Tag;
+Tag.propTypes = {
+    color: PropTypes.string,
+    closeable: PropTypes.bool,
+    afterClose: PropTypes.func,
+    circle: PropTypes.bool
+};
