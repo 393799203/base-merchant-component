7da11a4001e10a7f2750c177fa5322f560a67ca2
youyou <574350481@qq.com>
2017-03-15 11:23:37 +0800

    修改时间样式

--- public/pages/field/_module/raw/index.js
+++ public/pages/field/_module/raw/index.js
@@ -88,15 +88,9 @@ export default class RawView extends Component {
 				</div>
 
 				<div className='demo clearfix'>
-<<<<<<< 013ef1be7f2c17b682c4f2d45913fe5a4e1d3a16
-					<div className='f-l field-demo'>
-						<h5>必填属性：类型（type）、name、自定义表单体、表单复杂的情况下建议设置form给表单归类</h5>
-						<Field
-=======
 					<div className='fl field-demo'>
 						<h4>必填属性：类型（type）、name、自定义表单体、表单复杂的情况下建议设置form给表单归类</h4>
 						<Field 
->>>>>>> 调整组件样式
 						 	type="raw"
 	                        label="必填属性："
 	                        name="date"
@@ -110,15 +104,9 @@ export default class RawView extends Component {
 	                            showTime/>
 	                    </Field>
 					</div>
-<<<<<<< 013ef1be7f2c17b682c4f2d45913fe5a4e1d3a16
 					<div className="f-l field-demo">
-						<h5>其他配置属性：标题（label）、子标题信息（subInfo）、必填（required）、校验错误信息（errorMsg）、校验是否展示（error）</h5>
-						<Field
-=======
-					<div className="fl field-demo">
 						<h4>其他配置属性：标题（label）、子标题信息（subInfo）、必填（required）、校验错误信息（errorMsg）、校验是否展示（error）</h4>
 						<Field 
->>>>>>> 调整组件样式
 						 	type="raw"
 	                        label="配置属性1："
 	                        name="date2"
@@ -136,15 +124,9 @@ export default class RawView extends Component {
 	                            showTime/>
 	                    </Field>
 					</div>
-<<<<<<< 013ef1be7f2c17b682c4f2d45913fe5a4e1d3a16
-					<div className="f-l field-demo">
-						<h5>方法：将值填入表单中（onData）、校验（onValidate）、重置（onReset）、清空（onClear）</h5>
-						<Field
-=======
 					<div className="fl field-demo">
 						<h4>方法：将值填入表单中（onData）、校验（onValidate）、重置（onReset）、清空（onClear）</h4>
 						<Field 
->>>>>>> 调整组件样式
 						 	type="raw"
 	                        label="方法onData："
 	                        name="date3"
